4b14392f3492be6e11516a8e82b9a78db7df371c
Paweł Cesar Sanjuan Szklarz <paweld2@gmail.com>
2012-10-24 22:45:07 +0200

    c3-solve

--- workshop/guice/src/main/java/warsjava/guice/modules/ModelModule.java
+++ workshop/guice/src/main/java/warsjava/guice/modules/ModelModule.java
@@ -1,11 +1,14 @@
 package warsjava.guice.modules;
 
 import warsjava.guice.contract.LoggingContract;
+import warsjava.guice.contract.ModelContract;
 import warsjava.guice.implementations.LoggerLevel1;
 import warsjava.guice.implementations.LoggerLevel2;
+import warsjava.guice.implementations.ModelWarsjava;
 import warsjava.guice.implementations.SimpleLogger;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import com.google.inject.name.Names;
 
 public class ModelModule extends AbstractModule {
@@ -16,6 +19,8 @@ public class ModelModule extends AbstractModule {
 
 		bind(LoggingContract.class).annotatedWith(Names.named("level1")).to(LoggerLevel1.class);
 		bind(LoggingContract.class).annotatedWith(Names.named("level2")).to(LoggerLevel2.class);
+		
+		bind(ModelContract.class).to(ModelWarsjava.class).in(Singleton.class);
 	}
 
 }
