7149ee143ab0c24621a7bb7c29661d85ad0c5239
Paweł Cesar Sanjuan Szklarz <paweld2@gmail.com>
2012-10-24 22:25:22 +0200

    c2-solve

--- workshop/guice/src/main/java/warsjava/guice/modules/ModelModule.java
+++ workshop/guice/src/main/java/warsjava/guice/modules/ModelModule.java
@@ -1,15 +1,21 @@
 package warsjava.guice.modules;
 
 import warsjava.guice.contract.LoggingContract;
+import warsjava.guice.implementations.LoggerLevel1;
+import warsjava.guice.implementations.LoggerLevel2;
 import warsjava.guice.implementations.SimpleLogger;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 public class ModelModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
 		bind(LoggingContract.class).to(SimpleLogger.class);
+		
+		bind(LoggingContract.class).annotatedWith(Names.named("level1")).to(LoggerLevel1.class);
+		bind(LoggingContract.class).annotatedWith(Names.named("level2")).to(LoggerLevel2.class);
 	}
 
 }
