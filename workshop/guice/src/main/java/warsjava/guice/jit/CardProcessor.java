4a16f2406ec2493dbae086c483eda491b66b6729
Paweł Cesar Sanjuan Szklarz <paweld2@gmail.com>
2012-10-25 09:48:48 +0200

    c8-solve

--- workshop/guice/src/main/java/warsjava/guice/jit/CardProcessor.java
+++ workshop/guice/src/main/java/warsjava/guice/jit/CardProcessor.java
@@ -1,5 +1,8 @@
 package warsjava.guice.jit;
 
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(CardProcessorImpl.class)
 public interface CardProcessor {
 
 	public boolean processCard();
--- workshop/guice/src/main/java/warsjava/guice/jit/CardValidator.java
+++ workshop/guice/src/main/java/warsjava/guice/jit/CardValidator.java
@@ -1,5 +1,8 @@
 package warsjava.guice.jit;
 
+import com.google.inject.ProvidedBy;
+
+@ProvidedBy(CardValidatorProvider.class)
 public interface CardValidator {
 
 	public boolean validate();
