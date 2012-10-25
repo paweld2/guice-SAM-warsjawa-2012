c6e8dfe5f9924e5243b83040cce2ccb7a3772dda
Paweł Cesar Sanjuan Szklarz <paweld2@gmail.com>
2012-10-25 11:36:05 +0200

    c22-solve

--- workshop/guice/src/main/java/warsjava/guice/modules/TaskModelModule.java
+++ workshop/guice/src/main/java/warsjava/guice/modules/TaskModelModule.java
@@ -1,16 +1,20 @@
 package warsjava.guice.modules;
 
+import warsjava.guice.contract.InternalModelAPI;
 import warsjava.guice.domain.TaskObserver;
+import warsjava.guice.implementations.InternalModelApiImplementation;
 import warsjava.guice.implementations.TaskObserverImplementation;
 
-import com.google.inject.AbstractModule;
+import com.google.inject.PrivateModule;
 import com.google.inject.Scopes;
 
-public class TaskModelModule extends AbstractModule {
+public class TaskModelModule extends PrivateModule {
 
 	@Override
 	protected void configure() {
 		bind(TaskObserver.class).to(TaskObserverImplementation.class).in(Scopes.SINGLETON);
+		bind(InternalModelAPI.class).to(InternalModelApiImplementation.class);
+		expose(TaskObserver.class);
 	}
 
 }
